7dede4757ac91343bb99799a547f67464b937436
kaizersoje <54566217+kaizersoje@users.noreply.github.com>
2025-09-01 15:32:21 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1077,13 +1077,13 @@ class MiniGraphCard extends LitElement {
   }
 }
 
-customElements.define('mini-graph-card-new', MiniGraphCard);
+customElements.define('mini-graph-card', MiniGraphCard);
 
 // Configure the preview in the Lovelace card picker
 window.customCards = window.customCards || [];
 window.customCards.push({
-  type: 'mini-graph-card-new',
-  name: 'Mini Graph Card New',
+  type: 'mini-graph-card',
+  name: 'Mini Graph Card',
   preview: false,
   description: 'The Mini Graph card is a minimalistic and customizable graph card',
 });
